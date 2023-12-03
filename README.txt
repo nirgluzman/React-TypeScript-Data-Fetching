3a2e03adb540f753bc21d41964a02791d4666613
nirgluzman <nir.gluzman@gmail.com>
2023-12-03 16:51:31 +0100

    add Zod for data validation

--- README.txt
+++ README.txt
@@ -21,6 +21,12 @@ https://tanstack.com/query/latest
 Zod - data validation library
 https://zod.dev/
 https://github.com/colinhacks/zod
+https://www.udemy.com/course/react-typescript-the-practical-guide/learn/lecture/40470776#questions/20931846/
+When fetching data, it can be a good idea to parse & validate the fetched data to check 
+if it's in line with our data structure expectations.
+Zod is a great library for doing that valiation, because this library embraces TypeScript and 
+it is written such that TypeScript is able to infer the structure of the parsed / validated data.
+Zod installation: npm install zod
 
 
 {JSON} Placeholder - free fake API for testing and prototyping
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import {useEffect, useState, type ReactNode} from 'react';
 
+import {z} from 'zod';
+
 import BlogPosts, {type BlogPost} from './components/BlogPosts.tsx';
 import {get} from './util/http.ts';
 import fetchingImg from './assets/data-fetching.png';
@@ -12,24 +14,49 @@ type RawDataBlogPost = {
 	body: string;
 };
 
+// Zod schema for a single blog post.
+// We define it outside of App component function, since this doesn't need to be re-created all the time.
+const rawDataBlogPostSchema = z.object({
+	id: z.number(),
+	userId: z.number(),
+	title: z.string(),
+	body: z.string(),
+});
+
+// z.array() is a Zod method that creates a new schema based on another schema.
+// As the name suggests, it's simply an array containing the expected objects.
+const expectedResponseDataSchema = z.array(rawDataBlogPostSchema);
+
 function App() {
 	const [fetchedPosts, setFetchPosts] = useState<BlogPost[]>();
 
 	useEffect(() => {
 		async function fetchPosts() {
-			const rawData = (await get(
-				'https://jsonplaceholder.typicode.com/posts'
-			)) as RawDataBlogPost[];
+			// const rawData = (await get(
+			// 	'https://jsonplaceholder.typicode.com/posts'
+			// )) as RawDataBlogPost[];
 
-			const blogPosts: BlogPost[] = rawData.map((rawPost) => {
-				return {
-					id: rawPost.id,
-					title: rawPost.title,
-					text: rawPost.body,
-				};
-			});
+			try {
+				const rawData = await get('https://jsonplaceholder.typicode.com/posts');
 
-			setFetchPosts(blogPosts);
+				// Type casting with "as" is NOT needed!
+				// Instead, here, TypeScript "knows" that parsedData will be an array with objects as defined by the above schema.
+				// It will throw an error if rawData is not in line with the defined schema (e.g., if a property is missing or of a different value type).
+				// This is why we use try/catch block.
+				const parsedData = expectedResponseDataSchema.parse(rawData);
+
+				const blogPosts: BlogPost[] = parsedData.map((rawPost) => {
+					return {
+						id: rawPost.id,
+						title: rawPost.title,
+						text: rawPost.body,
+					};
+				});
+
+				setFetchPosts(blogPosts);
+			} catch (error) {
+				console.log(error);
+			}
 		}
 
 		fetchPosts();
